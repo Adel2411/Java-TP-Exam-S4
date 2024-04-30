80bf3e5c31a8fb7129daeb149d577fde570ab203
Adel2411 <hadjarabadel.2411@gmail.com>
2024-04-30 20:49:17 +0100

    Entire Project as presented

---
+++ src/Controller.java
@@ -0,0 +1,17 @@
+public class Controller {
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+
+        view.chiffreButton.addActionListener(e -> {
+            if (view.ValidateChiffre()) {
+                view.mounthTextField.setText(model.getMounth(view.getChiffre()));
+            } else {
+                view.showError();
+            }
+        });
+    }
+}
